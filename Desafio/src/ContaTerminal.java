bdbb0debf8d85017db9d352b5b15eda94b4f8f32
Giovanni Mendes <gmendes@tnsi.com>
2024-06-26 13:00:37 -0300

    Primeiro commit
    Fix bug

--- Desafio/src/ContaTerminal.java
+++ Desafio/src/ContaTerminal.java
@@ -10,6 +10,7 @@ public class ContaTerminal {
         
         System.out.println("Olá, informe o número: ");
         numero = scan.nextInt();
+        scan.nextLine();
         System.out.println("Informe a sua agência: ");
         agencia = scan.nextLine();
         System.out.println("Informe seu nome: ");
@@ -17,7 +18,7 @@ public class ContaTerminal {
         System.out.println("Informe seu saldo: ");
         saldo = scan.nextDouble();
 
-        System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
+        System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque.");
         
         scan.close();
     }
